8cf0054bfbee700b0638e224aa192091a7931006
webhomeschool <info@webhomeschool.de>
2013-07-15 16:54:04 +0200

    Revert "1.0.3 release"
    This reverts commit 52cea66e08ee9e76ad06c7cefd4b0d667cc58b25.

--- com_ablog_release_1.0.3/admin/sql/uninstall.mysql.utf8.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `#__ablog_categories`;
-
-DROP TABLE IF EXISTS `#__ablog_comments`;
-
-DROP TABLE IF EXISTS `#__ablog_comment_answers`;
-
-DROP TABLE IF EXISTS `#__ablog_posts`;
-
